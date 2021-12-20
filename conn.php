42ff3dfa1688818ca69620bcb14597b921a13435
brokieb <33734040+brokieb@users.noreply.github.com>
2021-12-20 18:02:23 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,12 +1,12 @@
 
 <?php
+$IP = "localhost";
+$DB = "DB";
+$PW = "DB_PASSWORD";
+$clientId = "ALLEGRO CLIENT ID";
+$clientSecret = "ALLEGRO SECRET";
 
-$conn = new mysqli("192.168.101.64","kajkosho_zestawy-skyshop","nyn%)[b%]8ZQ","kajkosho_skyshop_zestawy");
-//dane logowania allegro
-$clientId = "d60bbe2dfd4a47cc84f8aceefb0f676f";
-$clientSecret = "3BF9b1Up3gIjGAwERFiWR9Et8ORhcF8djwa67vXLuhTa9e6iQqOhSYyQchQNwUPJ";
-//koniec danych allegro
-
+$conn = new mysqli($IP,$DB,$PW,$DB);
 // Check connection
 if (!mysqli_set_charset($conn, "utf8mb4")) {
   printf("Error loading character set utf8mb4: %s\n", mysqli_error($conn));
